b45aa54641b32c3e73d4b78e905f77e28c55a6d4
hasmikchalabyan <46956638+hasmikchalabyan@users.noreply.github.com>
2019-12-17 02:48:24 +0400

    Update Histogram.java

--- Stage2/Task1/Histogram.java
+++ Stage2/Task1/Histogram.java
@@ -1,61 +1,42 @@
-import ij.IJ;
 import ij.ImagePlus;
-import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
+import ij.plugin.filter.PlugInFilter;
+import java.awt.Color;
+import ij.*;
 
-import java.awt.*;
-
-public class Histogram implements PlugInFilter {
-
-    //R G B
-    double[] red = new double[256];
-    double[] green = new double[256];
-    double[] blue = new double[256];
-
-    /**
-     * Set up
-     * @param args
-     * @param im
-     * @return
-     */
+public class normalized_histograms implements PlugInFilter {
     public int setup(String args, ImagePlus im) {
         return DOES_RGB;
     }
-
-
     public void run(ImageProcessor ip) {
-        for (int i = 0; i < ip.getWidth(); i++) {
-            for (int j = 0; j < ip.getHeight(); j++) {
-                Color color = new Color(ip.getPixel(i, j));
-                red[color.getRed()] += 1;
-                blue[color.getBlue()] += 1;
-                green[color.getGreen()] += 1;
+        int M = ip.getWidth();
+        int N = ip.getHeight();
+        float[] red = new float[256];
+        float[] green = new float[256];
+        float[] blue = new float[256];
+
+        Color color;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+               color = new Color(ip.getPixel(j,i));
+               red[color.getRed()] += 1;
+               green[color.getGreen()] += 1;
+               blue[color.getBlue()] += 1;
             }
         }
-
-        red[0] = red[0] / ip.getWidth() * ip.getHeight();
-        green[0] = green[0] / ip.getWidth() * ip.getHeight();
-        blue[0] = blue[0] / ip.getWidth() * ip.getHeight();
-
-        for (int i = 1; i < red.length; i++) {
-            red[i] = (red[i] / (ip.getWidth() * ip.getHeight())) + red[i - 1];
-            green[i] = (green[i] / (ip.getWidth() * ip.getHeight())) + green[i - 1];
-            blue[i] = (blue[i] / (ip.getWidth() * ip.getHeight())) + blue[i - 1];
-        }
-
-        IJ.log("Reds counts: ");
-        for (int i = 0; i < red.length; i++) {
-            IJ.log(Double.toString(red[i]));
-        }
-
-        IJ.log("Greens counts: ");
-        for (int i = 0; i < green.length; i++) {
-            IJ.log(Double.toString(green[i]));
-        }
-
-        IJ.log("Blues counts: ");
-        for (int i = 0; i < blue.length; i++) {
-            IJ.log(Double.toString(blue[i]));
-        }
+        for (int j = 1; j < red.length; j++) {
+            red[j] = red[j - 1] + red[j];
+            green[j] = green[j - 1] + green[j];
+            blue[j] = blue[j - 1] + blue[j];
+        } 
+    
+        for (int j = 0; j < red.length; j++) {
+            red[j] = red[j] / (M * N);
+            green[j] = green[j] / (M * N);
+            blue[j] = blue[j] / (M * N);
+            IJ.log(Float.toString(red[j]));
+            IJ.log(Float.toString(green[j]));
+            IJ.log(Float.toString(blue[j]));
+        } 
     }
 }
